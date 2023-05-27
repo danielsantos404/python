592718ee651e79fe1ec4ba1d636e029a791121a1
Daniel Santos <dan.danielfs@gmail.com>
2023-05-27 19:39:53 -0300

    Conversor de base decimal para base binária criado

---
+++ conversorDecimal-Binario/main.py
@@ -0,0 +1,35 @@
+print(f'''{'='*43}
+CONVERSOR DE BASE DECIMAL PARA BASE BINÁRIA
+{'='*43}''')
+
+while True:
+  dividendo = int(input("\nDigite um número inteiro e ele será convertido em BINÁRIO: "))
+  n_informado = dividendo
+  quociente = 1
+  lista = []
+
+  while quociente >= 1:
+    resto = dividendo%2
+    lista.insert(0,resto)
+    quociente = dividendo // 2
+    dividendo = quociente
+
+  binario = ''.join([str(item) for item in lista])
+  print("O número  →", n_informado, "←  vale  →", binario, '←  em binário!')
+
+  resposta = input('\nDeseja continuar? [S/N] ').upper()[0]
+  while resposta != 'S' and resposta != 'N':
+    resposta = input('Resposta invalida, use [S] ou [N]. ').upper()[0]
+  if resposta == 'N':
+    break
+print('''
+Volte sempre! :D''')
+
+print(f'''
+{'='*32}
+Alunos:
+Daniel Ferreira - 01504933
+Everton Figueirôa - 01177129
+Matheus Victor - 01515370
+Vinicius Rodrigues - 01519574
+{'='*32}''')
