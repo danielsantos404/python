285a029722b32cc36b6b7064500895993e64890e
Daniel Santos <dan.danielfs@gmail.com>
2023-05-31 09:31:25 -0300

    Mudança de tipo primitivo e adição de comentários
    Mudança de tipo primitivo no conversor de bases: int → float.
    
    Adição de comentários na aplicação para melhor compreensão do código.

--- conversorDecimal-Binario/main.py
+++ conversorDecimal-Binario/main.py
@@ -2,8 +2,9 @@ print(f'''{'='*43}
 CONVERSOR DE BASE DECIMAL PARA BASE BINÁRIA
 {'='*43}''')
 
+#Conversor de bases (Decimal → Binário).
 while True:
-  dividendo = int(input("\nDigite um número inteiro e ele será convertido em BINÁRIO: "))
+  dividendo = float(input("\nDigite um número e ele será convertido em BINÁRIO: "))
   n_informado = dividendo
   quociente = 1
   lista = []
@@ -17,11 +18,14 @@ while True:
   binario = ''.join([str(item) for item in lista])
   print("O número  →", n_informado, "←  vale  →", binario, '←  em binário!')
 
+#Confirmação se o usário deseja que a aplicação continue.
   resposta = input('\nDeseja continuar? [S/N] ').upper()[0]
   while resposta != 'S' and resposta != 'N':
     resposta = input('Resposta invalida, use [S] ou [N]. ').upper()[0]
   if resposta == 'N':
     break
+    
+#Resposta ao finalizar a aplicação.
 print('''
 Volte sempre! :D''')
 
